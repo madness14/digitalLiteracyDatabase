a167a47084f6c06aece48ad29f6835b2b071e281
ander pegueros <107764677+madness14@users.noreply.github.com>
2025-06-08 17:59:14 -0600

    Create 2.queriesCountries.sql

---
+++ queries/2.queriesCountries.sql
@@ -0,0 +1,57 @@
+-- América (10 países)
+INSERT INTO country (idCountry, idContinent, name, population) VALUES
+(211100, 211000, 'Mexico', 126200000),
+(211200, 211000, 'United States', 331000000),
+(211300, 211000, 'Colombia', 51300000),
+(211400, 211000, 'Brazil', 213000000),
+(211500, 211000, 'Argentina', 45380000),
+(211600, 211000, 'Canada', 38000000),
+(211700, 211000, 'Peru', 33000000),
+(211800, 211000, 'Venezuela', 28400000),
+(211900, 211000, 'Chile', 19400000),
+(211010, 211000, 'Ecuador', 17600000);
+
+-- África (10 países)
+INSERT INTO country (idCountry, idContinent, name, population) VALUES
+(212100, 212000, 'Nigeria', 206000000),
+(212200, 212000, 'Egypt', 102000000),
+(212300, 212000, 'South Africa', 59300000),
+(212400, 212000, 'Ethiopia', 115000000),
+(212500, 212000, 'Kenya', 53700000),
+(212600, 212000, 'Morocco', 36900000),
+(212700, 212000, 'Ghana', 31000000),
+(212800, 212000, 'Algeria', 43800000),
+(212900, 212000, 'Tanzania', 59700000),
+(212010, 212000, 'Sudan', 43800000);
+
+-- Europa (10 países)
+INSERT INTO country (idCountry, idContinent, name, population) VALUES
+(213100, 213000, 'Spain', 47300000),
+(213200, 213000, 'Germany', 83200000),
+(213300, 213000, 'France', 67300000),
+(213400, 213000, 'Italy', 59500000),
+(213500, 213000, 'United Kingdom', 67200000),
+(213600, 213000, 'Poland', 37800000),
+(213700, 213000, 'Ukraine', 43700000),
+(213800, 213000, 'Netherlands', 17500000),
+(213900, 213000, 'Portugal', 10300000),
+(213010, 213000, 'Sweden', 10300000);
+
+-- Asia (10 países)
+INSERT INTO country (idCountry, idContinent, name, population) VALUES
+(214100, 214000, 'China', 1402000000),
+(214200, 214000, 'India', 1380000000),
+(214300, 214000, 'Japan', 126000000),
+(214400, 214000, 'Indonesia', 273000000),
+(214500, 214000, 'Pakistan', 220000000),
+(214600, 214000, 'Bangladesh', 164000000),
+(214700, 214000, 'Russia', 146000000),
+(214800, 214000, 'Turkey', 84300000),
+(214900, 214000, 'Iran', 83900000),
+(214010, 214000, 'Thailand', 69900000);
+
+-- Oceanía (3 países)
+INSERT INTO country (idCountry, idContinent, name, population) VALUES
+(215100, 215000, 'Australia', 25600000),
+(215200, 215000, 'New Zealand', 5000000),
+(215300, 215000, 'Fiji', 896000);
