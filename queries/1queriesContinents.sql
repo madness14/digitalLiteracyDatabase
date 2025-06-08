8156349d6209b309f160f5100da425a1876cb45a
ander pegueros <107764677+madness14@users.noreply.github.com>
2025-06-08 17:51:19 -0600

    Create 1queriesContinents.sql

---
+++ queries/1queriesContinents.sql
@@ -0,0 +1,9 @@
+INSERT INTO continent (idContinent, name, population) VALUES
+(211000, 'America', 579000000), 
+(212000, 'Africa', 423000000), 
+(213000, 'Europa', 746000000), 
+(214000, 'Asia', 4641000000), 
+(215000, 'Oceania', 786000000);
+
+
+
